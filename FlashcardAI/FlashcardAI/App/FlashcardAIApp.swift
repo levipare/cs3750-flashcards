1647fc0006dca228203c9a1a8b0390c228a152a9
Levi Pare <levipare@gmail.com>
2025-10-16 14:55:31 -0400

    Simplified firebase configure

--- FlashcardAI/FlashcardAI/App/FlashcardAIApp.swift
+++ FlashcardAI/FlashcardAI/App/FlashcardAIApp.swift
@@ -9,20 +9,13 @@ import SwiftUI
 import FirebaseCore
 import FirebaseAuth
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
-
 @main
 struct FlashcardAIApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var authViewModel = AuthViewModel()
+    
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
